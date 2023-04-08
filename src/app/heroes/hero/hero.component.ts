3c7d548acd2ebf150c888e7da1326c574d41b595
jrejasyman <jrejas@vallegrande.edu.pe>
2023-04-08 15:33:28 -0500

    Heroe interface

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+export interface Heroe{
+  supername: string;
+  realname: string;
+  age: number;
+  country: string; 
+}
+
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
@@ -7,27 +14,42 @@ import { Component } from '@angular/core';
 })
 export class HeroComponent {
 
-  heroeName = 'Spiderman';
-  realName = 'Peter Parker';
-  age = 17;
-  country = 'Perú';
+  heroeSelected: Heroe = {
+    supername: 'Sin nombre',
+    realname: 'No tiene',
+    age: 0,
+    country: 'Sin país'
+  };
 
-  heroes = ['Spiderman','Ironman', 'Capitan America', 'Thor'];
+  heroes: Heroe[] =[
+    {
+      supername: 'Spiderman',
+      realname: 'Peter Parker',
+      age: 16,
+      country: 'Colombia'
+    },
+    {
+      supername: 'Ironman',
+      realname: 'Tony Stark',
+      age: 40,
+      country: 'Ecuador'
+    },
+    {
+      supername: 'Capitan America',
+      realname: 'Steve Rogers',
+      age: 80,
+      country: 'Bolivia'
+    },
+    {
+      supername: 'Thor',
+      realname: 'Thor',
+      age: 200,
+      country: 'Perú'
+    }
+  ];
 
-  get nameUppercase(){
-    return this.realName.toUpperCase();
-  }
 
-  anyFunction(){
-    return `${this.heroeName} -> ${this.age}`;
-  }  
-
-  changeHeroe(){
-    this.heroeName = 'Iron man';
-    this.realName = 'Tony Stark';
-  }
-
-  changeHeroList(heroName: string){
-    this.heroeName = heroName;
+  changeHero(hero: Heroe){
+    this.heroeSelected = hero;
   }
 }
